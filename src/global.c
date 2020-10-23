433469ef95557ff4bad8b625cfd024388197b113
MiguelYanez <miguel.yanez@alumnos.uv.cl>
2020-10-23 17:55:09 +0000

    commit

--- src/global.c
+++ src/global.c
@@ -1,2 +1,1 @@
 #include <global.hh>
-
---
+++ src/hora.c
@@ -0,0 +1,26 @@
+#include <global.hh>
+#include <unistd.h>
+
+void signal_handler( int signal_num ){
+        static uint32_t intentos = 0;
+        time_t t;
+        struct tm * timeinfo;
+        if(signal_num == 10){
+                time(&t);
+                timeinfo = localtime(&t);
+                std::cout << "Señal SIGUSR1 recibida: " << asctime(timeinfo);
+                std::cout << "Listo para recibir la señal SIGUSR1.\n";
+        }else intentos++;
+        if(intentos == 2){
+                exit(signal_num);
+        }
+}
+int main(int argc, char* argv[]){
+        std::cout << "Programa hora ejecutandose. PID=" << getpid() << ".\n";
+        std::cout << "Listo para recibir la señal SIGUSR1.\n";
+        std::signal(SIGUSR1, signal_handler);
+        std::signal(SIGINT, signal_handler);
+        while(true) {
+        }
+        return EXIT_SUCCESS;
+}
